f202f36b96d4a7e4cedb66f0fe5825d6aa5d13e3
Greg McCarthy <greg@gjmccarthy.co.uk>
2019-05-23 15:24:53 +0100

    Move to correct location

--- Python/convert.py
+++ Python/convert.py
@@ -1,68 +1,73 @@
+#!/usr/bin/env python3
+
 #Convert Commodore 64 binary input file from bin to ascii so it can
-#be used as a include file in an Arduino sketch  for writing to W27C512 EEPROM"
+#be used as a include file in an Arduino sketch  for writing to W27C512 EEPROM
 
 import binascii
 import sys
 import os
 
-if len(sys.argv) <> 2:
-        print "Script to convert  Commodore 64 file from  bin the ascii for  Arduino sketch"
-        print "Usage: convert.py inputfile"
+bytes=0
+
+if len(sys.argv) != 2:
+        print ("Script to convert  Commodore 64 file from  bin the ascii for  Arduino sketch")
+        print ("Usage: convert.py inputfile")
         exit()
 
 
 try:
-    file = open("bin_file.h","w")
+    file = open("bin_file.h","wb")
 except:
-    print "Unable to create bin_file.h"
+    print ("Unable to create bin_file.h")
     exit()
 
 try:
     statinfo = os.stat(sys.argv[1])
 except:
-    print "Unable to open inputfile", sys.argv[1]
+    print ("Unable to open inputfile", sys.argv[1])
     exit()
-if statinfo.st_size <> 8192:
-   print "Binary file is not 8192 bytes"
+
+if statinfo.st_size > 16384:
+   print ("Binary file greater than 16k")
    exit()
 
 
-file.write("const byte bin_file[] PROGMEM = { \\")
-file.write("\n")
-bytes = 0
+file.write("const byte bin_file[] PROGMEM = { \\".encode())
+file.write("\n".encode())
 try:
     with open(sys.argv[1], "rb") as f:
         b = True
         count=0
-
         while b:
-            if count < 16 and bytes < 8191:
+            if count < 16 and bytes < statinfo.st_size-1:
                 bytes = bytes + 1
                 hexdata = f.read(1)
-                file.write("0x")
+                file.write("0x".encode())
                 file.write(binascii.hexlify(hexdata))
-                file.write(",")
+                file.write(",".encode())
                 count = count + 1
-            elif bytes < 8191:
-               count = 0
-               file.write(" \\")
-               file.write("\n");
-            elif bytes > 8190:
+            elif bytes < statinfo.st_size-1:
+                count = 0
+                file.write(" \\".encode())
+                file.write("\n".encode());
+            elif bytes > statinfo.st_size-2:
                 bytes = bytes + 1
                 hexdata = f.read(1)
-                file.write("0x")
+                file.write("0x".encode())
                 file.write(binascii.hexlify(hexdata))
-                file.write(" \\")
-                file.write("\n")
-                file.write("};")
+                file.write(" \\".encode())
+                file.write("\n".encode())
+                file.write("};".encode())
                 b = False
 
-except:
-    print "Unable to open", sys.argv[1]
+except Exception as ex:
+    print ("Unable to open", sys.argv[1])
+    print(ex)
     file.close()
-    exit
+    exit()
 
 file.close()
 f.close()
-print "File converted. Created bin_file.h"
+print ("File converted. Created bin_file.h")
+
 
